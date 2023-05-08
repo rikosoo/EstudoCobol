4ac21ce9f39e83827d302f0386e11a8948e0e390
Henrique Stefanutti <52385414+rikosoo@users.noreply.github.com>
2023-05-07 23:36:06 -0300

    Calculadora
    Calcuradora que realiza operações simples.

---
+++ aplicacoes/calculadora.cbl
@@ -0,0 +1,78 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROGRAM-ID. CALCULADORA.
+       ENVIRONMENT DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       CONFIGURATION SECTION.
+      *-----------------------
+       INPUT-OUTPUT SECTION.
+      *-----------------------
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+        01 NUMEROINICIAL   PIC  9(5)            VALUE ZERO.
+        01 NUMEROFINAL      PIC  9(5)           VALUE ZERO.
+        01 RESULT PIC 9(5)                VALUE ZERO.
+        01 OPERADOR              PIC X VALUE "+".
+           88 DIVIDIR            VALUE "/".
+           88 SOMAR              VALUE "+".
+           88 SUBTRAIR           VALUE "-".
+           88 MULTIPLICAR        VALUE "*".
+           88 OPERADORESVALIDOS  VALUE "*"
+                                 , "+","/","-".
+        01 RESULTADO       PIC S9(5)V9(2) VALUE ZERO GLOBAL .
+
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "BEM-VINDO À CALCULADORA DO HENRICÃO".
+
+           DISPLAY "Coloque o primeiro numero".
+           ACCEPT NUMEROINICIAL.
+           DISPLAY "Qual operação deseja realizar? +, -, * e /.".
+           ACCEPT OPERADOR.
+           DISPLAY "Qual o segundo numero que deseja para a operação?".
+           ACCEPT NUMEROFINAL.
+           EVALUATE TRUE
+           WHEN DIVIDIR
+               DIVIDE NUMEROINICIAL INTO NUMEROFINAL
+               GIVING RESULTADO
+
+           WHEN SOMAR
+               ADD NUMEROINICIAL, NUMEROFINAL GIVING
+               RESULTADO
+
+           WHEN SUBTRAIR
+               SUBTRACT NUMEROINICIAL FROM NUMEROFINAL
+               GIVING RESULTADO
+
+           WHEN MULTIPLICAR
+               MULTIPLY NUMEROINICIAL BY NUMEROFINAL
+               GIVING RESULTADO
+
+
+           END-EVALUATE
+
+           IF OPERADORESVALIDOS
+               DISPLAY "RESULTADO ES = " RESULTADO
+           END-IF
+
+           STOP RUN.
+
+
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       MAIN-PROCEDURE.
+      **
+      * The main procedure of the program
+      **
+            DISPLAY "Hello world"
+            STOP RUN.
+      ** add other procedures here
+       END PROGRAM CALCULADORA.
